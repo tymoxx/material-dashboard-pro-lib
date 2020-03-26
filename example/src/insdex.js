a5b69ff8324fddca5ca05f5885fd2746ec8eeb95
tymoxx <7777kara@gmail.com>
2020-03-26 17:44:20 +0100

    clean up

--- example/src/insdex.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { render} from 'react-dom';
-import Example from "../../src/components/Test/Test";
-const App = () => (
-    <Example />
-);
-render(<App />, document.getElementById("root"));
